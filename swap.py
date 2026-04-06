e4cf66fd733fb2b5850088653569e075a2e45125
Rishita <ranarishita400@gmail.com>
2026-04-06 05:43:31 +0530

    Program 5: Swap the vlaues

---
+++ swap.py
@@ -0,0 +1,8 @@
+# Python program to swap two numbers
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+print("Before swapping: a = ",a,"b=",b)
+temp = a
+a = b
+b = temp
+print("After swapping: a=",a,"b = ",b)
