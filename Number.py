f7a13a97d82d82b4b8f67767813a6dd6a119c512
Rishita <ranarishita400@gmail.com>
2026-06-22 22:30:49 +0530

    if number is positive or negative

---
+++ Number.py
@@ -0,0 +1,8 @@
+# Checking if a number is positive,negative or Zero
+num = float(input("Enter any number:"))
+if num > 0 :
+    print("The number id positive")
+if num == 0:
+    print("Zero")
+if num < 0:
+    print("The number is negative")
